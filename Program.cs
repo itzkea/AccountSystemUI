fc99556c68ebe8abf50e52c880d4db047f2fc4bf
kea kea <arciagakatrina54@gmail.com>
2024-04-13 20:57:08 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UserAccountBusiness;
+
+namespace AccountSystemUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("ENTER EMAIL: ");
+            string email = Console.ReadLine();
+
+            Console.Write("ENTER PASSWORD: ");
+            string password = Console.ReadLine();
+
+            UserBusiness user = new UserBusiness(); 
+            bool result = user.VerifyUser(email, password);
+
+            if(result)
+            {
+                Console.WriteLine();
+                Console.WriteLine("SUCCESSFUL!!!");
+            }
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine("FAILED!!!");
+            }
+
+        }
+    }
+}
